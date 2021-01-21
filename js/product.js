538fb4aa7d92396fc30dfcc9f54a8ef4fb029c73
Viadsten <stanulevich.vg@yandex.ru>
2021-01-21 17:18:54 +0300

    add review and update basket-order

--- js/product.js
+++ js/product.js
@@ -7,7 +7,18 @@ $(document).ready(function() {
     let descrPlus = document.querySelectorAll('.descr-btn-plus--i');
     let bodyHeight = [];
     let bodyHeightAll = [];
+    let afterTop = $('.product-text-position')
+    let bodyHeightSorted;
 
+    function bodyFunction(){
+        
+    }
+    
+    bodyFunction();
+
+    window.onresize = function() {
+
+    }
     for (let i = 0; i < descrBtn.length; i++){
         if($(window).width() < 790) {
             $(descrBody[i]).slideUp(0);
@@ -27,24 +38,26 @@ $(document).ready(function() {
             descrPlus[i].classList.toggle('plus-rotate');
         }
           } else {
-            let afterTop = $('.product-text-position')
             bodyHeight[i] = $(descrBody[i]).height();
-            let bodyHeightSorted = bodyHeight;
+           
+            bodyHeightSorted = bodyHeight;
             min = bodyHeightSorted[0];
             max = min;
             for (let m = 1; m < bodyHeightSorted.length; m++) {
                 if (bodyHeightSorted[m] > max) max = bodyHeightSorted[m];
                 if (bodyHeightSorted[m] < min) min = bodyHeightSorted[m];
             }
-            $(afterTop).css('margin-top', (max - bodyHeight[0])* -1 + 30);
+            //$(afterTop).css('margin-top', (max - bodyHeight[0])* -1 + 30);
             descrBody[1].classList.add('descr-body--right');
             descrBody[2].classList.add('descr-body--right');
             
-            if(i != 0){
-                $(descrBody[i]).css('margin-top', bodyHeight[0] * -1 - 15);
-
+            if (i != 0){
+                $(descrBody[i]).css('margin-top', (bodyHeight[i - 1])* -1 - 15) ;
             }
+            $(afterTop).css('margin-top', (max - bodyHeight[0])* -1 + 30);
+
             descrBtn[i].onclick = function(){
+                
                 $(afterTop).css('margin-top', (max - bodyHeight[i])* -1 + 30);
                 removeRL(i);
                 if (i == 0){
@@ -59,7 +72,7 @@ $(document).ready(function() {
                     descrBody[2].classList.add('descr-body--right');
                 }
                 else if(i == 2){
-                    lineAnimate(640);
+                    lineAnimate(634);
                     descrBody[0].classList.add('descr-body--left');
                     descrBody[1].classList.add('descr-body--left');
                 }
@@ -72,7 +85,7 @@ $(document).ready(function() {
           }
         
     }
-
+   
     function lineAnimate(leftPos) {
         $('.prod-line-front').animate({
             left: leftPos
@@ -103,4 +116,73 @@ $(document).ready(function() {
     });
 
 
+
+
+
+//review
+
+let reviewText = document.querySelectorAll('.review-text');
+let reviewStar = document.querySelectorAll('.review-rating-wrp');
+let reviewUser = document.querySelectorAll('.prod-review-user--wrp');
+let reviewUserHeight = [];
+let reviewTextHeight = [];
+
+
+if($(window).width() > 600) { 
+    for (let i = 0; i < reviewText.length; i++){
+        reviewUserHeight[i] = $(reviewUser[i]).height();
+        reviewTextHeight[i] = $(reviewText[i]).height();
+        //console.log('text: ' + reviewTextHeight[i]);
+        //console.log('user: ' + reviewUserHeight[i]);
+        $(reviewStar[i]).css('margin-top', (reviewTextHeight[i] - reviewUserHeight[i])* (-1));
+    }
+}else {
+    for (let i = 0; i < reviewText.length; i++){
+        $(reviewStar[i]).css('margin-top', 10);
+        
+    }
+}
+
+    window.onresize = function() {
+        for (let i = 0; i < descrBtn.length; i++){
+            bodyHeight[i] = $(descrBody[i]).height();
+            bodyHeightSorted = bodyHeight;
+            min = bodyHeightSorted[0];
+            max = min;
+            for (let m = 1; m < bodyHeightSorted.length; m++) {
+                if (bodyHeightSorted[m] > max) max = bodyHeightSorted[m];
+                if (bodyHeightSorted[m] < min) min = bodyHeightSorted[m];
+            }
+            if (i != 0){
+                $(descrBody[i]).css('margin-top', (bodyHeight[i - 1])* -1 - 15) ;
+            }
+            
+
+            if (!$(descrBody[i]).hasClass('descr-body--left') && !$(descrBody[i]).hasClass('descr-body--right')){
+                $(afterTop).css('margin-top', (max - bodyHeight[i])* -1 + 30);
+            }
+        }
+       
+        //review
+
+        console.log('mi');
+    if($(window).width() > 600) { 
+        for (let i = 0; i < reviewText.length; i++){
+            reviewUserHeight[i] = $(reviewUser[i]).height();
+            reviewTextHeight[i] = $(reviewText[i]).height();
+            $(reviewStar[i]).css('margin-top', (reviewTextHeight[i] - reviewUserHeight[i])* (-1));
+            //console.log(reviewTextHeight[i] - reviewUserHeight[i]);
+        }
+    }
+    else{
+        for (let i = 0; i < reviewText.length; i++){
+            $(reviewStar[i]).css('margin-top', 10);
+            
+        }
+    }
+    }
 });
+
+
+
+
